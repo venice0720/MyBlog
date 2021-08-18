621d2b47c824827e09bbc421da674a8297803ffd
venice0720 <vincentluague07@gmail.com>
2021-08-19 01:19:59 +0800

    Completing the functionalites: CRUD without design

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,54 @@
+class ArticlesController < ApplicationController
+
+    def index
+        @articles = Article.all
+    end
+
+    def show
+         @article = Article.find(params[:id])
+    end
+
+    def find_name
+         @article = Article.find_by(name: params[:name])
+    end
+    
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            redirect_to articles_path
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+    end
+
+    def delete
+        @article=Article.find(params[:id])
+        @article.destroy
+        
+        redirect_to articles_path
+    end
+
+    private
+    
+    def article_params
+        params.require(:article).permit(:name, :body)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/articles' => 'articles#index'
+  get '/articles/new' => 'articles#new'
+  get '/articles/name/:name' => 'articles#find_name'
+  delete '/articles/delete/:id' => 'articles#delete', as: 'article_delete'
+  get '/articles/edit/:id' => 'articles#edit', as: 'article_edit'
+  get '/articles/:id' => 'articles#show'
+  put '/articles/update/:id' => 'articles#update', as: 'article_update'
+  post 'articles' => 'articles#create', as: 'article_create'
+  
 end
